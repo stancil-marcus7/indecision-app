93967f4d1256d74fd799c035b0ff51352881f240
Marcus Stancil <stancil.marcus@yahoo.com>
2019-08-08 09:47:48 -0400

    Convert components to stateless components

--- src/Options/Options.js
+++ src/Options/Options.js
@@ -1,44 +1,18 @@
 import React from  'react'
 import Option from './Option/Option'
 
-//Just an example of how methods need context
-const obj = {
-    name: 'Vikram',
-    getName() {
-        return this.name;
-    }
-}
+const Options = (props) => {
+    // Maps the options to the Option component
+    const options = props.options.map(option => {
+        return <Option key={option} message={option}/>;
+    })
 
-const getName = obj.getName.bind(obj);
-
-console.log(getName());
-
-class Options extends React.Component{
-    //We're going to override the constructor
-    constructor(props){
-        super(props);
-        //This will make sure handleRemoveAll will always have the correct context
-        this.handleRemoveAll = this.handleRemoveAll.bind(this)
-    }
-
-    //This method loses all context  when itis called(props will show up null if bind(this) isnt attached to its calls)
-    handleRemoveAll() {
-        console.log(this.props.options)
-    }
-
-    render(){
-        // Maps the options to the Option component
-        const options = this.props.options.map(option => {
-            return <Option key={option} message={option}/>;
-        })
-
-        return(
-            <div>
-                <button onClick={this.props.deleteOptions}>Remove All</button>
-                {options}
-            </div>
-        )
-    }
+    return(
+        <div>
+            <button onClick={props.deleteOptions}>Remove All</button>
+            {options}
+        </div>
+    )
 }
 
 export default Options;
