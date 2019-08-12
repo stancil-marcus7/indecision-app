fe28536e9079190f6e3ad67ee0ca9ea9b62605e0
Marcus Stancil <stancil.marcus@yahoo.com>
2019-08-12 10:43:52 -0400

    Add modal styling

--- src/OptionModal/OptionModal.js
+++ src/OptionModal/OptionModal.js
@@ -6,11 +6,13 @@ const OptionModal = (props) =>(
         //This converts props.selectedOption to a boolean value
         isOpen={!!props.selectedOption}
         onRequestClose={props.closeModal}
+        //Going to allow us to use our own styles for the modal
+        className="modal"
         closeTimeoutMS={200}
         contentLabel="Selected Option">
-        <h3>Selected Option</h3>
-        {props.selectedOption && <p>{props.selectedOption}</p>}
-        <button onClick={props.closeModal}>Okay</button>
+        <h3 className="modal__title">Selected Option</h3>
+        {props.selectedOption && <p className="modal__body">{props.selectedOption}</p>}
+        <button className="button" onClick={props.closeModal}>Okay</button>
     </Modal>
 )
 
